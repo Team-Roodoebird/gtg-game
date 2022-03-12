7ebd6c48abb19c0cef22aa30e6b6c7fdf07aa55f
Jeremiah Johnh <jeremiah.john@utdallas.edu>
2022-03-12 00:36:12 -0600

    Initial Commit
    Added gitignore and two already existing scripts. this is our starting point. previous initial commit was made by gitkraken

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] AudioClip _openingSong;
+    //[SerializeField] AudioClip _startingSong;
+    [SerializeField] Text _highScoreTextView = null;
+    [SerializeField] Slider volumeSlider = null;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+
+        //Load high score display
+        int highScore = PlayerPrefs.GetInt("HighScore");
+        _highScoreTextView.text = highScore.ToString();
+
+        //Play starting song on Menu Start
+        if (_openingSong != null)
+        {
+            global::AudioManager.Instance.PlaySong(_openingSong);
+        }
+
+        volumeSlider.value = global::AudioManager.Instance._audioSource.volume;
+        volumeSlider.maxValue = global::AudioManager.Instance._audioSource.maxDistance;
+        volumeSlider.maxValue = global::AudioManager.Instance._audioSource.minDistance;
+    }
+
+    public void AdjustVolume()
+    {
+        global::AudioManager.Instance.AdjustVolume(volumeSlider.value);
+        Debug.Log("New volume: " + volumeSlider.value.ToString());
+    }
+
+    public void ResetHighScore()
+    {
+        PlayerPrefs.SetInt("HighScore", 0);
+        Debug.Log("High score reset.");
+
+        int highScore = PlayerPrefs.GetInt("HighScore");
+        _highScoreTextView.text = highScore.ToString();
+    }
+
+    /*public void StartMusic()
+    {
+        global::AudioManager.Instance.PlaySong(_startingSong);
+    }*/
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
