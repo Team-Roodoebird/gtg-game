30847428969fad49a326af45a8a63bff7f059c0c
Nikita Istomin <nikita.istomin54@gmail.com>
2022-04-22 18:15:47 -0500

    Added basic Dialogue System
    - Testing script running the game
    - Printing out character by character, line after line
    - Override printing mode
    - TODO: Implement speaker name
    - TODO: Implement additive print mode

---
+++ Assets/Resources/TEST[DELETE BEFORE PUBLISH]/scripts/Testing.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    DialogueSystem dialogue;
+    public string[] testString = new string[]
+    {
+        "The FitnessGram Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues.:Anouncer",
+        "The 20 meter pacer test will begin in 30 seconds.",
+        "Line up at the start.",
+        "The running speed starts slowly but gets faster each minute after you hear this signal bodeboop.",
+        "A sing lap should be completed every time you hear this sound.",
+        "ding Remember to run in a straight line and run as long as possible.",
+        "The second time you fail to complete a lap before the sound, your test is over.",
+        "The test will begin on the word start. On your mark. Get ready!… Start. ding﻿"
+    };
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogue = DialogueSystem.instance;
+    }
+
+
+    int index = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Debug.Log("Space is pressed");
+            if(!dialogue.isSpeaking || dialogue.isWaitingForUserInput)
+            {
+
+                if (index >= testString.Length)
+                {
+                    return;
+                }
+                Say(testString[index]);
+                index++;
+            }
+        }
+    }
+
+
+    void Say(string s)
+    {
+        string[] parts = s.Split(':');
+        string speech = parts[0];
+        string speaker = (parts.Length >= 2) ? parts[1] : "";
+
+        dialogue.Say(speech, speaker);
+    }
+}
---
+++ Assets/Scripts/Core/DialogueSystem.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueSystem : MonoBehaviour
+{
+    //variable to have an easy reference to the dialogue system (assigned at awake)
+    public static DialogueSystem instance;
+
+    public ELEMENTS elements;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    /**
+     * Say something and show it on the speach box.
+     **/
+    public void Say(string speech, string speaker = "")
+    {
+        /*
+         * Psuedocode:
+         * 1. if we are currently talking, then stop talking
+         * 2. start saying the new stuff
+         */
+        StopSpeaking();
+        speaking = StartCoroutine(Speaking(speech, speaker));
+    }
+
+
+    public void StopSpeaking()
+    {
+        if (isSpeaking)
+        {
+            StopCoroutine(speaking);
+        }
+        speaking = null;
+    }
+
+
+    public bool isSpeaking { get { return speaking != null; } }
+    [HideInInspector] public bool isWaitingForUserInput = false;
+
+    Coroutine speaking = null;
+    IEnumerator Speaking(string targetSpeech, string speaker = "")
+    {
+        /*
+         * Psuedocode:
+         * 1. make sure the speach panel is visible on screen
+         * 2. clear out current text
+         * 3. make sure current speaker name is visible
+         * 4. while currently shown text != text we want to show
+         *      print the next character and return
+         * 5. wait for user input
+         */
+
+        speechPanel.SetActive(true);
+        speechText.text = "";
+        speakerNameText.text = speaker; // temporary, don't want to set speaker for every script line
+        isWaitingForUserInput = false;
+
+        while(speechText.text != targetSpeech)
+        {
+            speechText.text += targetSpeech[speechText.text.Length];
+            //finish for this frame and come back to this line on the next frame
+            yield return new WaitForEndOfFrame();
+        }
+
+        //text finished
+        isWaitingForUserInput = true;
+        while (isWaitingForUserInput)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+        StopSpeaking();
+    }
+
+
+
+
+
+
+    /**
+     * The main panel containing all dialogue related elements on the UI
+     **/
+    [System.Serializable]
+    public class ELEMENTS
+    {
+        public GameObject speechPanel;
+        public Text speakerNameText;
+        public Text speechText;
+    }
+
+    public GameObject speechPanel { get { return elements.speechPanel; } }
+    public Text speakerNameText { get { return elements.speakerNameText; } }
+    public Text speechText { get { return elements.speechText; } }
+}
