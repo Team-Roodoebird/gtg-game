c7a70de86011f00bedab154988555807edf83a3f
Nikita Istomin <nikita.istomin54@gmail.com>
2022-04-22 16:03:53 -0500

    Clean up, Reorganization, & Minor Overall fixes
    - Prefabs created
    - Aspect ration and resolution scaling implemented
    - Main menu & dialog box anchors set
    - Text wrapping fixed
    - Text size and scaling improved
    - Main scene renamed "unity" -> "Game" 
        > And moved to the Scenes folder

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,56 +1,56 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
 
-public class MainMenuController : MonoBehaviour
-{
-    [SerializeField] AudioClip _openingSong;
-    //[SerializeField] AudioClip _startingSong;
-    [SerializeField] Text _highScoreTextView = null;
-    [SerializeField] Slider volumeSlider = null;
+//public class MainMenuController : MonoBehaviour
+//{
+//    [SerializeField] AudioClip _openingSong;
+//    //[SerializeField] AudioClip _startingSong;
+//    [SerializeField] Text _highScoreTextView = null;
+//    [SerializeField] Slider volumeSlider = null;
 
-    private void Start()
-    {
-        Cursor.lockState = CursorLockMode.None;
+//    private void Start()
+//    {
+//        Cursor.lockState = CursorLockMode.None;
 
-        //Load high score display
-        int highScore = PlayerPrefs.GetInt("HighScore");
-        _highScoreTextView.text = highScore.ToString();
+//        //Load high score display
+//        int highScore = PlayerPrefs.GetInt("HighScore");
+//        _highScoreTextView.text = highScore.ToString();
 
-        //Play starting song on Menu Start
-        if (_openingSong != null)
-        {
-            global::Audio.Instance.PlaySong(_openingSong);
-        }
+//        //Play starting song on Menu Start
+//        if (_openingSong != null)
+//        {
+//            global::Audio.Instance.PlaySong(_openingSong);
+//        }
 
-        volumeSlider.value = global::Audio.Instance._audioSource.volume;
-        volumeSlider.maxValue = global::Audio.Instance._audioSource.maxDistance;
-        volumeSlider.maxValue = global::Audio.Instance._audioSource.minDistance;
-    }
+//        volumeSlider.value = global::Audio.Instance._audioSource.volume;
+//        volumeSlider.maxValue = global::Audio.Instance._audioSource.maxDistance;
+//        volumeSlider.maxValue = global::Audio.Instance._audioSource.minDistance;
+//    }
 
-    public void AdjustVolume()
-    {
-        global::Audio.Instance.AdjustVolume(volumeSlider.value);
-        Debug.Log("New volume: " + volumeSlider.value.ToString());
-    }
+//    public void AdjustVolume()
+//    {
+//        global::Audio.Instance.AdjustVolume(volumeSlider.value);
+//        Debug.Log("New volume: " + volumeSlider.value.ToString());
+//    }
 
-    public void ResetHighScore()
-    {
-        PlayerPrefs.SetInt("HighScore", 0);
-        Debug.Log("High score reset.");
+//    public void ResetHighScore()
+//    {
+//        PlayerPrefs.SetInt("HighScore", 0);
+//        Debug.Log("High score reset.");
 
-        int highScore = PlayerPrefs.GetInt("HighScore");
-        _highScoreTextView.text = highScore.ToString();
-    }
+//        int highScore = PlayerPrefs.GetInt("HighScore");
+//        _highScoreTextView.text = highScore.ToString();
+//    }
 
-    /*public void StartMusic()
-    {
-        global::Audio.Instance.PlaySong(_startingSong);
-    }*/
+//    /*public void StartMusic()
+//    {
+//        global::Audio.Instance.PlaySong(_startingSong);
+//    }*/
 
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
-}
+//    public void QuitGame()
+//    {
+//        Application.Quit();
+//    }
+//}
