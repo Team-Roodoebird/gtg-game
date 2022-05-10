b9fd561436d17e69343dd2b377cb0b810bb0ad14
Jeremiah Johnh <jeremiah.john@utdallas.edu>
2022-05-09 23:43:20 -0500

    added new script to control game menu
    all it does is turn the menu on or off when escape key is pressed. must find best way to make use of script functionality (how it will control actual menu options)

---
+++ Assets/Scripts/GameMenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenuController : MonoBehaviour
+{
+    public GameObject gameMenu;
+    // Start is called before the first frame update
+    /*void Start()
+    {
+        
+    }*/
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Escape Key Pressed");
+            gameMenu.SetActive(!gameMenu.activeSelf);
+        }
+    
+    }
+}
